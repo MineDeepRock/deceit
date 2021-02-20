7b8266f97b81eb2c471f8f967f21a386c92ecd12
suinua <maatinosippo@gmail.com>
2021-02-20 17:12:34 +0900

    add GameStorage

---
+++ src/deceit/storages/GameStorage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace deceit\storages;
+
+
+use deceit\models\Game;
+use deceit\models\GameId;
+
+class GameStorage
+{
+    static array $games;
+
+    static function add(Game $game): void {
+        self::$games[] = $game;
+    }
+
+    static function delete(GameId $gameId): void {
+
+        foreach (self::$games as $key => $game) {
+            if ($game->getId()->equals($gameId)) unset(self::$games[$key]);
+        }
+
+        self::$games = array_values(self::$games);
+    }
+
+    static function deleteAll():void {
+        self::$games = [];
+    }
+
+    static function findById(GameId $gameId): ?Game {
+
+        foreach (self::$games as $game) {
+            if ($game->getId()->equals($gameId)) {
+                return $game;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return array|Game[]
+     */
+    static function getAll(): array {
+        return self::$games;
+    }
+}
