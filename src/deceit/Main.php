cbd23fe92b15c320973aa446e9b64e517183b3a9
suinua <maatinosippo@gmail.com>
2021-02-20 18:03:40 +0900

    update

--- src/deceit/Main.php
+++ src/deceit/Main.php
@@ -2,11 +2,24 @@
 
 namespace deceit;
 
+use deceit\dao\PlayerStatusDAO;
+use deceit\models\PlayerStatus;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase
+class Main extends PluginBase implements Listener
 {
     public function onLoad() {
         DataFolderPath::init($this->getDataFolder());
+    }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        $playerName = $event->getPlayer()->getName();
+
+        if (PlayerStatusDAO::findByName($playerName) === null) {
+            $status = new PlayerStatus($playerName);
+            PlayerStatusDAO::save($status);
+        }
     }
 }
