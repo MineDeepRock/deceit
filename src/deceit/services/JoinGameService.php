a810f2c3b8ef83e7e454002de1a45bd4bd22dbd3
suinua <maatinosippo@gmail.com>
2021-02-20 18:07:37 +0900

    update

--- src/deceit/services/JoinGameService.php
+++ src/deceit/services/JoinGameService.php
@@ -4,15 +4,19 @@
 namespace deceit\services;
 
 
+use deceit\dao\PlayerStatusDAO;
 use deceit\models\GameId;
+use deceit\models\PlayerStatus;
 use deceit\storages\GameStorage;
 
 class JoinGameService
 {
     static function execute(GameId $gameId, string $playerName): void {
         $game = GameStorage::findById($gameId);
-        if ($game !== null) {
-            $game->addPlayer($playerName);
-        }
+        if ($game === null) return;
+
+        //Status更新
+        $result = $game->addPlayer($playerName);
+        if ($result) PlayerStatusDAO::update(new PlayerStatus($playerName, $gameId));
     }
 }
