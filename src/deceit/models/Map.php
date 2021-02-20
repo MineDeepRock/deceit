28b5e2d5d1dde69af529669e4e8abe12a1796319
suinua <maatinosippo@gmail.com>
2021-02-20 17:04:21 +0900

    add Map

---
+++ src/deceit/models/Map.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace deceit\models;
+
+
+use pocketmine\math\Vector3;
+
+class Map
+{
+    private string $name;
+
+    private Vector3 $startVector;
+    private array $fuelTankVectors;
+    private array $fuelSpawnVectors;
+
+    public function __construct(string $name, Vector3 $startVector, array $fuelTankVectors, array $fuelSpawnVectors) {
+        $this->name = $name;
+        $this->startVector = $startVector;
+        $this->fuelTankVectors = $fuelTankVectors;
+        $this->fuelSpawnVectors = $fuelSpawnVectors;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * @return Vector3
+     */
+    public function getStartVector(): Vector3 {
+        return $this->startVector;
+    }
+
+    /**
+     * @return Vector3[]
+     */
+    public function getFuelTankVectors(): array {
+        return $this->fuelTankVectors;
+    }
+
+    /**
+     * @return Vector3[]
+     */
+    public function getFuelSpawnVectors(): array {
+        return $this->fuelSpawnVectors;
+    }
+}
