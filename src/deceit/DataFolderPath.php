98021cf0fcc774c344ae81d44e614c8b1226adf3
suinua <maatinosippo@gmail.com>
2021-02-20 18:03:34 +0900

    update

--- src/deceit/DataFolderPath.php
+++ src/deceit/DataFolderPath.php
@@ -7,10 +7,13 @@ namespace deceit;
 class DataFolderPath
 {
     static string $map;
+    static string $playerStatus;
 
     static function init(string $path) {
         self::$map = $path . "maps/";
-
         if (!file_exists(self::$map)) mkdir(self::$map);
+
+        self::$playerStatus = $path . "player_status/";
+        if (!file_exists(self::$playerStatus)) mkdir(self::$playerStatus);
     }
 }
