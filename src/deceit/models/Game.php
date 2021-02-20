1166a50b79beb9aa625c764a0cea40ab817c1af1
suinua <maatinosippo@gmail.com>
2021-02-20 17:33:04 +0900

    update Game

--- src/deceit/models/Game.php
+++ src/deceit/models/Game.php
@@ -6,6 +6,8 @@ namespace deceit\models;
 class Game
 {
     private GameId $gameId;
+    private string $gameOwnerName;
+
     private int $maxPlayers;
     private int $wolfsCount;
 
@@ -17,15 +19,69 @@ class Game
     private Map $map;
     private Timer $timer;
 
-    private function __construct(GameId $gameId, int $maxPlayers, int $wolfsCount) {
+    private bool $isStarted;
+
+    private function __construct(GameId $gameId, string $gameOwnerName, int $maxPlayers, int $wolfsCount, array $playersName, array $wolfsName, array $fuelTanks, Map $map, Timer $timer, bool $isStarted) {
         $this->gameId = $gameId;
+        $this->gameOwnerName = $gameOwnerName;
         $this->maxPlayers = $maxPlayers;
         $this->wolfsCount = $wolfsCount;
+        $this->playersName = $playersName;
+        $this->wolfsName = $wolfsName;
+        $this->fuelTanks = $fuelTanks;
+        $this->map = $map;
+        $this->timer = $timer;
+        $this->isStarted = $isStarted;
     }
 
-    static function asNew(int $maxPlayers, int $wolfsCount): self {
-        return new Game(GameId::asNew(), $maxPlayers, $wolfsCount);
+    static function asNew(string $gameOwnerName, Map $map, Timer $timer, int $maxPlayers, int $wolfsCount): self {
+
+        $fuelTanks = [];
+        foreach ($map->getFuelSpawnVectors() as $fuelSpawnVector) {
+            $fuelTanks[] = FuelTank::asNew();
+        }
+
+        return new Game(
+            GameId::asNew(),
+            $gameOwnerName,
+            $maxPlayers,
+            $wolfsCount,
+            [],
+            [],
+            $fuelTanks,
+            $map,
+            $timer,
+            false
+        );
     }
 
+    public function addPlayer(string $playerName): bool {
+        if (in_array($playerName, $this->playersName)) return false;
+        if (count($this->playersName) === $this->maxPlayers) return false;
+        if ($this->isStarted) return false;
 
+        $this->playersName[] = $playerName;
+        return true;
+    }
+
+    public function removePlayer(string $playerName): bool {
+        if (!in_array($playerName, $this->playersName)) return false;
+
+        $index = array_search($playerName, $this->playersName);
+        unset($this->playersName[$index]);
+        $this->playersName = array_values($this->playersName);
+
+        if ($playerName === $this->gameOwnerName) {
+            $this->gameOwnerName = $this->playersName[0];
+        }
+
+        return true;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
 }
