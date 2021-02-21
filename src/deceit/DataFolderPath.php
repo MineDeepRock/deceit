3fc44d54a8b84f6f93c646f0583ef1a667e0ba77
suinua <maatinosippo@gmail.com>
2021-02-21 17:18:28 +0900

    add path

--- src/deceit/DataFolderPath.php
+++ src/deceit/DataFolderPath.php
@@ -8,6 +8,8 @@ class DataFolderPath
 {
     static string $map;
     static string $playerStatus;
+    static string $skin;
+    static string $geometry;
 
     static function init(string $path) {
         self::$map = $path . "maps/";
