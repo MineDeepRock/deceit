19597b7869c38a0c04d6a4ca0170087263e088e1
suinua <maatinosippo@gmail.com>
2021-02-21 17:18:35 +0900

    add FuelTankEntity

---
+++ src/deceit/pmmp/entities/FuelTankEntity.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace deceit\pmmp\entities;
+
+
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\ListTag;
+
+class FuelTankEntity extends EntityBase
+{
+    const NAME = "FuelTank";
+
+    public string $geometryId = "geometry." . self::NAME;
+    public string $geometryName = self::NAME . ".geo.json";
+
+    public function __construct(Level $level, Position $position) {
+        parent::__construct($level, $nbt = new CompoundTag('', [
+            'Pos' => new ListTag('Pos', [
+                new DoubleTag('', $position->getX()),
+                new DoubleTag('', $position->getY()),
+                new DoubleTag('', $position->getZ())
+            ]),
+            'Motion' => new ListTag('Motion', [
+                new DoubleTag('', 0),
+                new DoubleTag('', 0),
+                new DoubleTag('', 0)
+            ]),
+            'Rotation' => new ListTag('Rotation', [
+                new FloatTag("", 0),
+                new FloatTag("", 0)
+            ]),
+        ]));
+    }
+
+}
