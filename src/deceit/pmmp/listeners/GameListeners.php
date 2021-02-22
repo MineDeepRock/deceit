f0478b32c676c4a71ac4b1ebc31c37761c3881c9
suinua <maatinosippo@gmail.com>
2021-02-22 23:25:13 +0900

    投票機能の実装

--- src/deceit/pmmp/listeners/GameListeners.php
+++ src/deceit/pmmp/listeners/GameListeners.php
@@ -4,20 +4,26 @@ namespace deceit\pmmp\listeners;
 
 
 use deceit\dao\PlayerStatusDAO;
+use deceit\models\PlayerStatus;
 use deceit\pmmp\entities\CadaverEntity;
 use deceit\pmmp\entities\FuelTankEntity;
 use deceit\pmmp\events\FuelTankBecameFullEvent;
+use deceit\pmmp\events\VotedPlayerEvent;
+use deceit\pmmp\forms\ConfirmVoteForm;
 use deceit\pmmp\items\FuelItem;
 use deceit\storages\GameStorage;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDeathEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
 
+//TODO:蘇生機能
 class GameListeners implements Listener
 {
     public function onDamagedFuelTankEntity(EntityDamageEvent $event) {
@@ -82,15 +88,10 @@ class GameListeners implements Listener
         }
     }
 
-    public function onGamePlayerDeath(PlayerDeathEvent $event) :void {
+    public function onGamePlayerDeath(PlayerDeathEvent $event): void {
         $player = $event->getPlayer();
         $playerStatus = PlayerStatusDAO::findByName($player->getName());
-        $gameId = $playerStatus->getBelongGameId();
-        if ($gameId === null) return;
-
-        $game = GameStorage::findById($gameId);
-        if (!$game->isStarted()) return;
-        if ($game->isFinished()) return;
+        if (!$this->belongGameIsInProgress($playerStatus)) return;
 
         $player->setSpawn($player->getPosition());
         $cadaver = new CadaverEntity($player->getLevel(), $player);
@@ -100,14 +101,93 @@ class GameListeners implements Listener
     public function onGamePlayerRespawn(PlayerRespawnEvent $event): void {
         $player = $event->getPlayer();
         $playerStatus = PlayerStatusDAO::findByName($player->getName());
-        $gameId = $playerStatus->getBelongGameId();
-        if ($gameId === null) return;
-
-        $game = GameStorage::findById($gameId);
-        if (!$game->isStarted()) return;
-        if ($game->isFinished()) return;
+        if (!$this->belongGameIsInProgress($playerStatus)) return;
 
         $player->setGamemode(Player::SPECTATOR);
         $player->setImmobile(true);
+    }
+
+    public function onTapCadaverEntity(EntityDamageByEntityEvent $event) {
+        $cadaverEntity = $event->getEntity();
+        $attacker = $event->getDamager();
+        if (!($attacker instanceof Player)) return;
+        if (!($cadaverEntity instanceof CadaverEntity)) return;
+        $event->setCancelled();
+
+        //死体のオーナーとタップした人の確認
+
+        //持ち主がオフライン
+        $cadaverEntityOwner = $cadaverEntity->getOwner();
+        if (!$cadaverEntityOwner->isOnline()) return;
+
+        //進行中のゲームに参加しているか
+        $cadaverEntityOwnerStatus = PlayerStatusDAO::findByName($cadaverEntityOwner->getName());
+        $cadaverEntityOwnerGameId = $cadaverEntityOwnerStatus->getBelongGameId();
+        $attackerStatus = PlayerStatusDAO::findByName($attacker->getName());
+        $attackerGameId = $attackerStatus->getBelongGameId();
+
+        if (!$this->belongGameIsInProgress($attackerStatus)) return;
+        if (!$this->belongGameIsInProgress($cadaverEntityOwnerStatus)) return;
+
+        //同じゲームに属しているか
+        if (!$attackerGameId->equals($cadaverEntityOwnerGameId)) return;
+
+        $attacker->sendForm(new ConfirmVoteForm($cadaverEntity));
+    }
+
+    public function onVotedPlayerEvent(VotedPlayerEvent $event): void {
+        $cadaverEntity = $event->getCadaverEntity();
+        $votedPlayerNameList = $cadaverEntity->getVotedPlayerNameList();
+
+        $player = $cadaverEntity->getOwner();
+        $playerStatus = PlayerStatusDAO::findByName($player->getName());
+        if (!$this->belongGameIsInProgress($playerStatus)) return;
+
+        $game = GameStorage::findById($playerStatus->getBelongGameId());
+
+        $isMajority = (count($game->getAlivePlayerNameList()) - count($votedPlayerNameList) * 2) <= 0;
+
+        if ($isMajority) {
+            $cadaverEntity->kill();
+        }
+    }
+
+
+    public function onCadaverDeath(EntityDeathEvent $event) {
+        $event->setDrops([]);
+        $entity = $event->getEntity();
+
+        if (!($entity instanceof CadaverEntity)) return;
+
+        //TODO:本当の死、スペクテイターにする
+        $owner = $entity->getOwner();
+        if (!$owner->isOnline()) return;
+
+        $ownerStatus = PlayerStatusDAO::findByName($owner->getName());
+        $belongGameId = $ownerStatus->getBelongGameId();
+        if ($belongGameId === null) return;
+        $owner->setGamemode(Player::SPECTATOR);
+
+        $game = GameStorage::findById($belongGameId);
+        foreach ($game->getPlayersName() as $name) {
+            $player = Server::getInstance()->getPlayer($name);
+            if ($player === null) return;
+            if (!$player->isOnline()) return;
+
+            $player->sendMessage(TextFormat::RED . $owner->getName() . "が処刑されました");
+            $player->sendTitle(TextFormat::RED . $owner->getName() . "が処刑されました");
+        }
+    }
+
+
+    private function belongGameIsInProgress(PlayerStatus $playerStatus): bool {
+        $gameId = $playerStatus->getBelongGameId();
+        if ($gameId === null) return false;
+
+        $game = GameStorage::findById($gameId);
+        if (!$game->isStarted()) return false;
+        if ($game->isFinished()) return false;
+
+        return true;
     }
 }
