eb39dbf341b6675fe4391136cdefdea770c07923
suinua <maatinosippo@gmail.com>
2021-02-23 12:19:41 +0900

    add BossBarTypeList

---
+++ src/deceit/pmmp/BossBarTypeList.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace deceit\pmmp;
+
+
+use bossbar_system\model\BossBarType;
+
+class BossBarTypeList
+{
+    static function GameTimer(): BossBarType {
+        return new BossBarType("GameTimer");
+    }
+
+    static function ExitTimer(): BossBarType {
+        return new BossBarType("ExitTimer");
+    }
+}
