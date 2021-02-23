1eb031bd456824021b0412a992363759f15f07cc
suinua <maatinosippo@gmail.com>
2021-02-23 23:37:18 +0900

    add commands

--- src/deceit/Main.php
+++ src/deceit/Main.php
@@ -4,12 +4,17 @@ namespace deceit;
 
 use deceit\dao\PlayerStatusDAO;
 use deceit\models\PlayerStatus;
+use deceit\pmmp\forms\CreateGameForm;
+use deceit\pmmp\forms\SettingGameForm;
 use deceit\pmmp\listeners\GameListener;
 use deceit\pmmp\scoreboards\LobbyScoreboard;
 use deceit\services\QuitGameService;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener
@@ -35,5 +40,20 @@ class Main extends PluginBase implements Listener
         $playerName = $event->getPlayer()->getName();
 
         QuitGameService::execute($playerName);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($sender instanceof Player) {
+            if ($label === "create") {
+                $sender->sendForm(new CreateGameForm($this->getScheduler()));
+                return true;
+            }
+            if ($label === "setting") {
+                $sender->sendForm(new SettingGameForm());
+                return true;
+            }
+        }
+
+        return false;
     }
 }
