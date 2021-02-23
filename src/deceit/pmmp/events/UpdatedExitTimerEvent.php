70ca053fb7b3375e370be5198bcd17b73e4de3c3
suinua <maatinosippo@gmail.com>
2021-02-23 12:05:08 +0900

    add events

---
+++ src/deceit/pmmp/events/UpdatedExitTimerEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace deceit\pmmp\events;
+
+
+use deceit\models\GameId;
+use pocketmine\event\Event;
+
+class UpdatedExitTimerEvent extends Event
+{
+
+    private GameId $gameId;
+
+    public function __construct(GameId $gameId) {
+        $this->gameId = $gameId;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
+}
