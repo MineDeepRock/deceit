78a3f2fd6467be173c8d78b8c064d780a0b2003e
suinua <maatinosippo@gmail.com>
2021-02-26 18:41:04 +0900

    add slot menu

---
+++ src/deceit/pmmp/slot_menus/ExitVectorSettingSlotMenu.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace deceit\pmmp\slot_menus;
+
+
+use deceit\dao\MapDAO;
+use deceit\models\Map;
+use deceit\pmmp\forms\MapSettingForm;
+use pocketmine\block\Block;
+use pocketmine\item\ItemIds;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use slot_menu_system\SlotMenu;
+use slot_menu_system\SlotMenuElement;
+
+class ExitVectorSettingSlotMenu extends SlotMenu
+{
+
+    private Map $map;
+
+    public function __construct(Map $map) {
+        $this->map = $map;
+        parent::__construct(
+            [
+                new SlotMenuElement(
+                    ItemIds::DIAMOND_SWORD,
+                    "タップで出口を設定",
+                    function (Player $player) {
+                        $updatedMap = $this->updateMap($player);
+
+                        $player->sendForm(new MapSettingForm($updatedMap));
+                    },
+                    function (Player $player, Block $block) {
+                        $updatedMap = $this->updateMap($block);
+
+                        $player->sendForm(new MapSettingForm($updatedMap));
+                    }
+                )
+            ]
+        );
+    }
+
+
+    private function updateMap(Vector3 $vector3): Map {
+        $newMap = new Map(
+            $this->map->getLevelName(),
+            $this->map->getName(),
+            $this->map->getStartVector(),
+            $vector3,
+            $this->map->getOriginalExitBlockId(),
+            $this->map->getFuelTankVectors(),
+            $this->map->getFuelSpawnVectors(),
+        );
+        MapDAO::update($newMap);
+
+        return MapDao::findByName($this->map->getName());
+    }
+}
