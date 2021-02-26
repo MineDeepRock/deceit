6d77863bcce9f8e7851bce132975d96cda7cc1ec
suinua <maatinosippo@gmail.com>
2021-02-26 19:32:17 +0900

    add service

---
+++ src/deceit/services/CreateNewMapService.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace deceit\services;
+
+
+use deceit\dao\MapDAO;
+use deceit\models\Map;
+use pocketmine\block\BlockIds;
+use pocketmine\math\Vector3;
+
+class CreateNewMapService
+{
+    static function execute(string $levelName, string $mapName, Vector3 $spawnPoint): bool {
+        if ($levelName === null or $mapName === null) return false;
+
+        $newMap = new Map(
+            $levelName,
+            $mapName,
+            $spawnPoint,
+            $spawnPoint,
+            BlockIds::STONE,
+            [],
+            []
+        );
+
+        MapDAO::save($newMap);
+
+        return true;
+    }
+}
