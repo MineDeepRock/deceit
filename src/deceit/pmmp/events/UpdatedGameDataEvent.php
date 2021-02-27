de5eb9bc53f3a3a2c2dfa9663f9a13c08f8a1a9d
suinua <maatinosippo@gmail.com>
2021-02-28 00:33:59 +0900

    ゲームの内容が更新された際のイベントを追加

---
+++ src/deceit/pmmp/events/UpdatedGameDataEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace deceit\pmmp\events;
+
+
+use deceit\types\GameId;
+use pocketmine\event\Event;
+
+class UpdatedGameDataEvent extends Event
+{
+    private GameId $gameId;
+
+    public function __construct(GameId $gameId) {
+        $this->gameId = $gameId;
+    }
+
+    /**
+     * @return GameId
+     */
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
+}
